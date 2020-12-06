ae6e775e774eb5dc0d74fd90fbc2ff6211e10155
pavlin-dimitrov <74736322+pavlin-dimitrov@users.noreply.github.com>
2020-12-06 15:58:39 +0200

    Update HW4_Zad4.php

--- HOME_WORKS/HW4_PHP/HW4_Zad4.php
+++ HOME_WORKS/HW4_PHP/HW4_Zad4.php
@@ -1,13 +1,24 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form action="HW4_Zad4.php" method = "post">
+		<input type="number" name="month">
+		<input type="submit">
+	</form>
 
 
-
-<form action="HW4_Zad4.php" method = "post">
-	<input type="number" name="month">
-	<input type="submit">
-</form>
-
-<? php
+<?php
 
 $months = array( 1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "Novembre", 12 => "December");
-	each $months[$_POST["month"]];			 
+	echo $months[$_POST["month"]];
+
+	?>
+
+
 	
+</body>
+</html>
